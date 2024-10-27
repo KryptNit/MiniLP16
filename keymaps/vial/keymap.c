7653e64af77a7699c49a56e2bd6bba6065982124
KryptNit <meoxwklktr@gmail.com>
2024-10-27 17:01:19 +0700

    Rev keymap

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -4,49 +4,75 @@
 #include QMK_KEYBOARD_H
 
 enum layer_name {
-    _MAKRO,
-    _CAL,
+    _A,
+    _B,
+    _C,
+    _D
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-        TO(_CAL),                      KC_MUTE, 
-        KC_Q,   KC_W,   KC_E,   KC_R,
-        KC_A,   KC_S,   KC_D,   KC_F,
-        KC_Z,   KC_X,   KC_C,   KC_V,
-        KC_Y,   KC_U, KC_I, KC_O
-    ),
-
-    [1] = LAYOUT(
-        TO(_MAKRO),                      KC_MUTE, 
+        TO(_B),                      KC_MUTE, 
         KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
         KC_P4,   KC_P5,   KC_P6,   KC_PAST,
         KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
         KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+    ),
+
+    [1] = LAYOUT(
+        TO(_C),                      KC_MUTE, 
+        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
+        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
+        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+    ),
+
+    [2] = LAYOUT(
+        TO(_D),                         KC_TRNS, 
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS
+    ),
+
+    [3] = LAYOUT(
+        TO(_A),                         KC_TRNS, 
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS
     )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(KC_P1, KC_P2), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] =   { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+    [0] =   { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1] =   { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [2] =   { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [3] =   { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
 };
 #endif
 
 #ifdef  OLED_ENABLE
 
 bool oled_task_user(void) {
-    
-    oled_write_P(PSTR("Layer: "), false);
+    oled_set_cursor(3, 2);
+    oled_write_P(PSTR("Layer:  "), false);
 
     switch (get_highest_layer(layer_state)) {
-        case _MAKRO:
-            oled_write_P(PSTR("Based\n"), false);
+        case _A:
+            oled_write_P(PSTR("Web3\n"), false);
             break;
-        case _CAL:
-            oled_write_P(PSTR("Cal\n"), false);
+        case _B:
+            oled_write_P(PSTR("NumPad\n"), false);
+            break;
+        case _C:
+            oled_write_P(PSTR("Func\n"), false);
+            break;
+        case _D:
+            oled_write_P(PSTR("Misc\n"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
